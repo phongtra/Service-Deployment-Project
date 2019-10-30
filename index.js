fda72757cf53426e76b501a93e0301b3653a8123
Phongtran1998 <hoang_phong98@me.com>
2019-10-30 22:19:36 +0200

    third commit

--- index.js
+++ index.js
@@ -33,5 +33,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoute')(app);
 require('./routes/mainRoute')(app);
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up the production assets
+  app.use(express.static(__dirname + '/client/build'));
+  //Express will serve up the index.html if
+  //it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 4000);
